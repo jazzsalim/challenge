6f8dc94bd2c75f72b29b0b4cee768959a82ec475
jazzsalim <salimjaziri855@gmail.com>
2023-02-07 12:35:10 +0100

    first commit

---
+++ WebApplication5/WebApplication5/Models/personne.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+namespace WebApplication5.Models
+
+{
+    public class personne
+    {
+        string Guid  prenomID { get; set; }
+        string nom { get; set; }
+        string email { get; set; }
+        string description { get; set;}
+    
+
+        public personne(string prenomID, string nom, string email, string description)
+        {
+            prenomID = prenomID;
+            nom = nom;
+            email = email;
+            description = description;
+
+        }
+    }
+}
---
+++ WebApplication5/WebApplication5/data/TutoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication5.Models;
+
+namespace WebApplication5.data
+{
+    public class TutoDbContext : DbContext
+    {
+        public TutoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<personne> personnes {get ; set;}
+    }
+}
